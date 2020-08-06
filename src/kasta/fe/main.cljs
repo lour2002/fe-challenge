15d91610c62dd5b927512735f53c7873fa0b6fd4
lour2002 <boris.avs@live.ru>
2020-08-06 23:17:31 +0300

    Save code

--- src/kasta/fe/main.cljs
+++ src/kasta/fe/main.cljs
@@ -1,19 +1,27 @@
 (ns kasta.fe.main
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [rum.core :as rum]
-            [aleph.http :as http]
+            [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]))
 
 (enable-console-print!)
 
 (go 
-  (let [response (http/get "https://kasta.ua/api/v2/campaigns"))]
-    (def campaignsData (:body response))
-    (println campaignsData)
-))
+  (let [response (<! (http/get "/api/campaigns" {:with-credentials? false}))]
+      (def tagsList (:menu (:body response)))
+  )
+)
+
+(rum/defc tagItem
+  [data]
+  [:div {:key (get data :url)}(get data :name)]
+  )
 
 (rum/defc Root []
-  [:div {:class "container"} response ])
+  [:div {:class "container"} 
+    (mapv tagItem tagsList) 
+  ]
+)
 
 
 (defn ^:export trigger-render []
